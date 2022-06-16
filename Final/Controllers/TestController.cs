1dcb96d691089c48bedc1ef2efba1adabd76f502
liang-liang-la <connie11607@yahoo.com>
2022-06-16 19:10:04 +0800

    第四題

---
+++ Final/Controllers/TestController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Final.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Exam()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Music()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Music(String name,String num)
+        {
+            if (name.IsNullOrWhiteSpace()) {
+                ViewBag.NameMessage = "請輸入姓名";
+            }
+            if (num.IsNullOrWhiteSpace())
+            {
+                ViewBag.NameMessage = "請輸入數字";
+            }
+
+            if (num == "1")
+            {
+                ViewBag.Result = "do";
+            }
+            else if (num == "2") {
+                ViewBag.Result = "re";
+            }
+            else if (num == "3")
+            {
+                ViewBag.Result = "mi";
+            }
+            else if (num == "4")
+            {
+                ViewBag.Result = "fa";
+            }
+            else if (num == "5")
+            {
+                ViewBag.Result = "so";
+            }
+            else if (num == "6")
+            {
+                ViewBag.Result = "la";
+            }
+            else if (num == "7")
+            {
+                ViewBag.Result = "xi";
+            }
+            else
+            {
+                ViewBag.Result = "錯誤";
+            }
+
+            ViewBag.Name = name;
+            return View();
+        }
+
+    }
+}
